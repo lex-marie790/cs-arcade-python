15e53cf09144842c97bc199444bef622a68a0989
lex-marie790 <lexi.marie790@gmail.com>
2021-02-10 22:13:52 -0800

    added more python problems

---
+++ GetCommit.py
@@ -0,0 +1,28 @@
+"""
+Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+The Abanamama Version System (AVS) is a software versioning and revision control system used in highly secure environments. In this system, each commit is assigned a unique name, the first part of which consists of the username encrypted in the base-4 system using symbols '0', '?', '+', and '!', and the second part consists of symbols of English alphabet.
+
+Given such commit, your task is go remove the username part from it and return the second part as an answer.
+
+Example
+
+For commit = "0??+0+!!someCommIdhsSt", the output should be
+getCommit(commit) = "someCommIdhsSt".
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] string commit
+
+Commit in the format given above. Note, that it is possible that it doesn't contain the first or the second part.
+
+Guaranteed constraints:
+5 ≤ commit.length ≤ 45.
+
+[output] string
+
+The second part of the commit.
+"""
+def getCommit(commit):
+    return commit.translate({ord:(i): None for i in '0?!+'})
